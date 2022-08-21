13f33c652b5cb8537fef9f16ade573bb75a00fa1
bolo-ong <wldnjscoals@gmail.com>
2022-08-21 23:18:36 +0900

    threeSixNine complete

--- threeSixNine.js
+++ threeSixNine.js
@@ -1,5 +1,8 @@
 function threeSixNine(n){
-    if(n % 3 == 0){
+    if(n % 9 == 0){
+        console.log('박수x2')
+    }
+    else if(n % 3 == 0){
         console.log('박수');
     }else{
         console.log('통과');
@@ -7,3 +10,5 @@ function threeSixNine(n){
 }
 
 threeSixNine(3);
+threeSixNine(9);
+threeSixNine(1);
